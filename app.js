f4820c0b11ba3449a25d4b53879be6dff95ffecc
HappyKiller <007kostiv@gmail.com>
2020-11-12 13:33:09 +0200

    +add filter for ol, which do not work

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ const addBtn = document.getElementById('add-btn');
 const list = document.querySelector('.list');
 const items = list.children;
 const countNum = document.querySelector('.task-counter_num');
+const filterSelector = document.querySelector('.filter-selection');
 
 // Визначення завдань на початку роботи сторінки
 countNum.innerText = items.length;
+console.log(document.cookie);
 
 function addTask() {
   // Ініціалізація елементів
   const newTask = document.createElement('li');
-  newTask.classList.add('list_item');
+  newTask.classList.add('list_elem');
 
   const taskCheckbox = document.createElement('input');
   taskCheckbox.classList.add('list_elem_txt');
@@ -47,5 +49,34 @@ function addTask() {
   countNum.innerText = items.length;
 }
 
+function filterTodo(e) {
+  const todos = list.childNodes;
+  const taskTxt = document.querySelector('.list_elem_txt');
+  const tasks = document.getElementsByClassName('.list_elem');
+
+  todos.forEach(() => {
+    switch (e.target.value) {
+      case "all":
+        tasks.style.display = 'block';
+        break;
+      case 'completed':
+        if (taskTxt.classList.contains('list_elem_txt__checked')) {
+          tasks.style.display = 'block';
+        } else {
+          tasks.style.display = 'none';
+        }
+        break;
+      case 'uncompleted':
+        if (!taskTxt.classList.contains('list_elem_txt__checked')) {
+          tasks.style.display = 'block';
+        } else {
+          tasks.style.display = 'none';
+        }
+    }
+  })
+}
+
+
+filterSelector.addEventListener('click', filterTodo);
 // Виконання фунції при натисканні кнопки
-addBtn.addEventListener('click', () => { addTask() });
+addBtn.addEventListener('click', addTask);
